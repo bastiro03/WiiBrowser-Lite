efb1b14d616cda7439d80fde4ee4d97ef6bf5f0f
Gavethebest@gmail.com <Gavethebest@gmail.com>
2013-02-15 07:11:37 +0000

    Added missing files
    git-svn-id: svn://localhost/trunk@38 1a4c2bc4-540c-48e5-8c71-7a1e3271b4ae

--- source/httplib.cpp
+++ source/httplib.cpp
@@ -32,10 +32,21 @@
 #include <stdlib.h>
 
 #include "httplib.h"
+#include "main.h"
 
 // -----------------------------------------------------------
 // VARIABLES
 // -----------------------------------------------------------
+
+const char Agents[MAXAGENTS][256] =
+{
+    "",
+    "libcurl-agent/1.0",
+    "Mozilla/5.0 (Nintendo Wii; U; WiiBrowser rev37) like Gecko Firefox/4.0",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1",
+	"Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)",
+	"Mozilla/5.0 (Linux; U; Android 1.1; en-gb; dream) AppleWebKit/525.10+ (KHTML, like Gecko) Version/3.0.4 Mobile Safari/523.12.2",
+};
 
 const struct block emptyblock = {NULL, 0};
 
@@ -59,7 +70,7 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
     size_t realsize = size * nmemb;
     struct MemoryStruct *mem = (struct MemoryStruct *)userp;
-    mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+    mem->memory = (char *)realloc(mem->memory, mem->size + realsize + 1);
     if (mem->memory == NULL) {
         printf("not enough memory (realloc returned NULL)\n");
         exit(EXIT_FAILURE);
@@ -76,7 +87,7 @@ struct block downloadfile(CURL *curl_handle, const char *url, FILE *hfile)
     int res;
 
     struct MemoryStruct chunk;
-    chunk.memory = malloc(1);   /* will be grown as needed by the realloc above */
+    chunk.memory = (char *)malloc(1);   /* will be grown as needed by the realloc above */
     chunk.size = 0; /* no data at this point */
 
     if(curl_handle) {
@@ -91,8 +102,7 @@ struct block downloadfile(CURL *curl_handle, const char *url, FILE *hfile)
 
         /* some servers don't like requests that are made without a user-agent
         field, so we provide one */
-        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "Mozilla/5.0 (Linux; U; Android 1.1; en-gb; dream) AppleWebKit/525.10+ (KHTML, like Gecko) Version/3.0.4 Mobile Safari/523.12.2");
-		// curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, Agents[Settings.UserAgent]);
         curl_easy_setopt(curl_handle, CURLOPT_SSL_VERIFYPEER, 0L);
 
         /* proper function to close sockets */
@@ -111,7 +121,7 @@ struct block downloadfile(CURL *curl_handle, const char *url, FILE *hfile)
 
         curl_easy_setopt(curl_handle, CURLOPT_URL, url);
         if ((res=curl_easy_perform(curl_handle)) != 0) {   /*error!*/
-            Debug(url); Debug(curl_easy_strerror(res));
+            Debug(url); Debug(curl_easy_strerror((CURLcode)res));
             return emptyblock;
         }
 
